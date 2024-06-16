5ebe8141e35be2e86768e403768012452117fdb1
Valentina <96546273+valentinagiusti@users.noreply.github.com>
2024-06-16 20:05:19 -0300

    feat(api): add basic CRUD endpoints

---
+++ server/src/user/user.controller.ts
@@ -0,0 +1,54 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from '@prisma/client';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getUsers(): Promise<User[]> {
+    return this.userService.getAllUsers();
+  }
+
+  @Post()
+  async createUser(@Body() user: User): Promise<User> {
+    return this.userService.createUser(user);
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string): Promise<User> {
+    const user = await this.userService.getUserById(Number(id));
+    if (!user) {
+      throw new BadRequestException('User not found');
+    } else {
+      return user;
+    }
+  }
+
+  @Delete(':id')
+  async deleteUserById(@Param('id') id: string): Promise<User> {
+    try {
+      return this.userService.deleteUser(Number(id));
+    } catch (error) {
+      throw new BadRequestException('User does not exist');
+    }
+  }
+  @Put(':id')
+  async updateUser(@Param('id') id: string, @Body() user: User): Promise<User> {
+    try {
+      return this.userService.updateUser(Number(id), user);
+    } catch (error) {
+      throw new BadRequestException('User does not exist');
+    }
+  }
+}
